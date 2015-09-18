e03db821638155ae74dde19ff17f70df23546911
dcamposliz <dcampos.liz@gmail.com>
2015-09-18 15:23:15 -0700

    added separate script and style files

---
+++ script.js
@@ -0,0 +1,11 @@
+/* 
+This Javascript needs to take the value of the Learn More input search box, 
+and post it to "http://www.example.com?searchkeyword='{value}'".
+It can also dynamically build out the form below if that is easy to do or 
+needs to be done in order for the script to work.
+*/
+
+function searchFunction(string){
+	// search some website given passed string parameter
+}
+
